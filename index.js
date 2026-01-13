6db5630546523d3409b53e22adad601a406d417d
SprkSmR <chitox_ji_armen@hotmail.com>
2026-01-13 07:46:25 -0600

    Pretty much done, just requires styling and layout

--- index.js
+++ index.js
@@ -4,11 +4,21 @@ import axios from "axios";
 const app = express();
 
 const PORT = 3000;
+const URL = "https://randomfox.ca/floof/";
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
+});
+
+app.get("/get-fox", async (req, res) => {
+    try {
+        const response = await axios.get(URL);
+        res.render("fox.ejs", { image: response.data["image"]})
+    } catch(error){
+        res.render("error.ejs", { errorMessage: error.response.data});
+    }
 });
 
 app.listen(PORT, () => {
